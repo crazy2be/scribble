448ecbb92c027954f820942f7a85e614b4f29495
crazy2be <code@jmcgirr.com>
2017-06-26 12:58:33 -0600

    Very basic menu popup.

--- main.js
+++ main.js
@@ -88,6 +88,22 @@ window.addEventListener('load', function() {
         canvas.onmousemove = null;
         sock.send('tnone');
     };
+    var menu = new radialMenu({spacing: 0});
+    menu.add("🗑"); // Trash can => delete
+    menu.add("🖌"); // Paint brush
+    menu.add("Fill");
+    colors = [
+        "#FFF","#C1C1C1","#EF130B","#FF7100","#FFE400","#00CC00",
+        "#00B2FF","#231FD3","#A300BA","#D37CAA","#A0522D",
+        "#000","#4C4C4C","#740B07","#C23800","#E8A200","#005510",
+        "#00569E","#0E0865","#550069","#A75574","#63300D"]
+    for (var i = 0; i < colors.length; i++) {
+        menu.add("", {"size": 0.5, "background-style": "fill: " + colors[i]});
+    }
+    canvas.oncontextmenu = function(ev) {
+        menu.open();
+        ev.preventDefault();
+    };
     var guess = document.querySelector('#chat-input textarea');
     guess.onkeypress = function(ev) {
         if (ev.keyCode == 13) {
